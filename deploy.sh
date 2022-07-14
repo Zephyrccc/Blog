36fe0c3ebc0381b79583f4fbdd202874e5030994
Zephyr <ZephyrChen@outlook.com>
2022-07-14 16:21:23 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ cd docs/.vuepress/dist
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:Zephyrccc/Zephyrccc.github.io.git
+  githubUrl=git@github.com:Zephyrccc/Blog.git
 else
   msg='来自github actions的自动部署'
   githubUrl=https://Zephyrccc:${GITHUB_TOKEN}@github.com/Zephyrccc/Zephyrccc.github.io.git
